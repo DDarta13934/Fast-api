775511d1d7841b09a7ccae1d7eb1748f0a70842f
Andrey <balamut551161010@gmail.com>
2026-04-17 15:33:00 +0300

    Update ui.py

--- app/routers/ui.py
+++ app/routers/ui.py
@@ -1,17 +1,18 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
-from db import get_conn
+# Исправленный импорт
+from app.db import get_conn
 from psycopg2.extras import RealDictCursor
 
 router = APIRouter()
-templates = Jinja2Templates(directory="templates")
+# Исправленный путь к папке (app/templates)
+templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/")
 async def read_root(request: Request):
     conn = get_conn()
     try:
         with conn.cursor(cursor_factory=RealDictCursor) as cur:
-            # Берем только ID и ФИО для списка
             cur.execute('SELECT id, "ФИО_обучающегося" FROM students ORDER BY id')
             students = cur.fetchall()
             return templates.TemplateResponse("index.html", {
