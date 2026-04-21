111eee564d3cfe23c6617719ab1b6d4ebd27f05e
Andrey <balamut551161010@gmail.com>
2026-04-21 21:30:12 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,13 +3,10 @@ from fastapi.staticfiles import StaticFiles
 from app.routers.students import router as students_router
 from app.routers.ui import router as ui_router
 
-# Настраиваем redirect_slashes=False, чтобы /students и /students/ работали одинаково
+# Параметр redirect_slashes=False вылечит ошибку 404 для /students/
 app = FastAPI(redirect_slashes=False)
 
 app.include_router(ui_router)
-# Подключаем студентов
 app.include_router(students_router)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# УДАЛИ отсюда @app.get("/get_student"), он мешает!
